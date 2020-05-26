6d71484a901abf7fe8420cc5cb766f2d13f9ebea
Ashfaaq Mohammed <iim.ashfaaq@gmail.com>
2020-05-26 12:51:48 +0530

    Few examples update

---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            DefConstructor dc = new DefConstructor();
+            dc.DoThis();
+
+            AddConstructor ac = new AddConstructor();
+            AddConstructor ac1 = new AddConstructor(b:6, a:10);
+
+            StaticMethod.Acc();
+            StaticMethod sm = new StaticMethod();
+            sm.Slow();
+        }
+    }
+
+    public class DefConstructor
+    {
+        public void DoThis()
+        {
+            Console.WriteLine("DoThis has run...");
+        }
+    }
+
+    public class AddConstructor
+    {
+        public AddConstructor()
+        {
+            Console.WriteLine("The constructor has been called");
+            DoThat();
+        }
+
+        public AddConstructor(int a, int b = 10)
+        {
+            Console.WriteLine("Add : " + (a + b));
+        }
+
+        public void DoThat()
+        {
+            Console.WriteLine("DoThat had run...");
+        }
+    }
+
+
+    public class StaticMethod
+    {
+        #region "Static method"
+        public static void Acc()
+        {
+            Console.WriteLine("Acc...");
+        }
+        #endregion
+
+        public void Slow()
+        {
+            Console.WriteLine("Slow...");
+        }
+    }
+}
---
+++ EventHandler/EventHandler/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace EventExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            //Instatiate an event publisher object
+            EvtPublisher ep = new EvtPublisher();
+
+            //Instantiate an event subscriber object
+            EvtSubscriber es = new EvtSubscriber();
+
+            //publisher event pointing to subscriber target handler
+            ep.evt += es.HandleEvent;
+
+            ep.CheckBalance(300);
+        }
+    }
+
+    public class EvtPublisher
+    {
+        //public delegate void del(string x);
+
+        //public event del evt;
+
+        public EventHandler evt;
+        //pre-wired delegate
+        //public delegate void EventHandler(object sender, EventArgs e);
+
+        public void CheckBalance(int x)
+        {
+            if (x > 250)
+                evt(this, EventArgs.Empty);
+        }
+    }
+
+    public class EvtSubscriber
+    {
+        public void HandleEvent(object sender, EventArgs e)
+        {
+            Console.WriteLine("Sender " + sender + " above 250...");
+        }
+    }
+}
